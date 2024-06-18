55e2ade08871e88b3e4387dc864b429c67563f7a
Alan Ibarra <alanjibarradev@gmail.com>
2024-06-18 15:08:52 -0700

    add custom colors for landing page

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -2,7 +2,27 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        landing: {
+          blue: {
+            100: "#00218F",
+            200: "#152C5B",
+          },
+          turquoise: "#37E2D5",
+          gray: {
+            50: "#969696",
+            100: "#C4C4C4",
+            200: "#343434",
+            300: "#0C0047",
+          },
+          purple: {
+            100: "#F2F2FF",
+            200: "#6A4BFF",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
