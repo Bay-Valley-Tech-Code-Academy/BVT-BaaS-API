f2f12813ac20ff536133b5c74c4b0689e7530a35
Manuel Manriquez <manriquezmanny@gmail.com>
2024-06-21 13:52:42 -0700

    Setteled on 24px for icons, text is text-base, and got rid of redundant react imports

--- front-end/src/components/SideNavBar.jsx
+++ front-end/src/components/SideNavBar.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import { Home, Users, Settings, LogOut, KeyRound } from "lucide-react";
 
@@ -8,7 +7,7 @@ export default function SideNavBar() {
       <NavLink to="/dashboard">
         <div className="text-xl text-slate-700">
           <div className="mt-1 flex items-center rounded-md p-2.5 transition-colors hover:text-slate-700">
-            <h1 className="ml-4 text-[15px] font-bold">SaaS-Name</h1>
+            <h1 className="ml-4 text-base font-bold">SaaS-Name</h1>
           </div>
           <div className="my-2 h-[1px] bg-gray-300"></div>
         </div>
@@ -17,13 +16,13 @@ export default function SideNavBar() {
         {({ isActive }) => (
           <div className="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-dashboard-gray-50 transition-colors duration-300 hover:bg-blue-50 hover:text-slate-700">
             <span className={isActive ? "text-purple-700" : ""}>
-              <Home size={27} />
+              <Home size={24} />
             </span>
             <span
               className={
                 isActive
-                  ? "ml-4 text-[15px] font-bold text-purple-700"
-                  : "ml-4 text-[15px] font-bold"
+                  ? "ml-4 text-base font-bold text-purple-700"
+                  : "ml-4 text-base font-bold"
               }
             >
               Home
@@ -35,13 +34,13 @@ export default function SideNavBar() {
         {({ isActive }) => (
           <div className="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-dashboard-gray-50 transition-colors duration-300 hover:bg-blue-50 hover:text-slate-700">
             <span>
-              <Users size={27} className={isActive ? "text-purple-700" : ""} />
+              <Users size={24} className={isActive ? "text-purple-700" : ""} />
             </span>
             <span
               className={
                 isActive
-                  ? "ml-4 text-[15px] font-bold text-purple-700"
-                  : "ml-4 text-[15px] font-bold"
+                  ? "ml-4 text-base font-bold text-purple-700"
+                  : "ml-4 text-base font-bold"
               }
             >
               Users
@@ -54,15 +53,15 @@ export default function SideNavBar() {
           <div className="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-dashboard-gray-50 transition-colors duration-300 hover:bg-blue-50 hover:text-slate-700">
             <span>
               <KeyRound
-                size={27}
+                size={24}
                 className={isActive ? "text-purple-700" : ""}
               />
             </span>
             <span
               className={
                 isActive
-                  ? "ml-4 text-[15px] font-bold text-purple-700"
-                  : "ml-4 text-[15px] font-bold"
+                  ? "ml-4 text-base font-bold text-purple-700"
+                  : "ml-4 text-base font-bold"
               }
             >
               API Keys
@@ -75,15 +74,15 @@ export default function SideNavBar() {
           <div className="mt-3 flex cursor-pointer items-center rounded-md p-2.5 px-4 text-dashboard-gray-50 transition-colors duration-300 hover:bg-blue-50 hover:text-slate-700">
             <span>
               <Settings
-                size={27}
+                size={24}
                 className={isActive ? "text-purple-700" : ""}
               />
             </span>
             <span
               className={
                 isActive
-                  ? "ml-4 text-[15px] font-bold text-purple-700"
-                  : "ml-4 text-[15px] font-bold"
+                  ? "ml-4 text-base font-bold text-purple-700"
+                  : "ml-4 text-base font-bold"
               }
             >
               Settings
@@ -94,9 +93,9 @@ export default function SideNavBar() {
       <div className="my-4 h-[1px] bg-gray-300"></div>
       <div className="mt-1 flex cursor-pointer items-center justify-self-end rounded-md p-2.5 px-4 text-dashboard-gray-50 transition-colors duration-300 hover:bg-blue-50 hover:text-red-400">
         <span>
-          <LogOut size={27} />
+          <LogOut size={24} />
         </span>
-        <span className="ml-4 text-[15px] font-bold">Logout</span>
+        <span className="ml-4 text-base font-bold">Logout</span>
       </div>
     </div>
   );
