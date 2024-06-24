e7612aaddec630a44982c5da84ec4b7c2f0b4a4a
Manuel Manriquez <manriquezmanny@gmail.com>
2024-06-24 12:40:46 -0700

    Users Component with Hard Coded Dummy Data complete

---
+++ front-end/src/components/Inactive.jsx
@@ -0,0 +1,9 @@
+export default function Inactive() {
+  return (
+    <div className="flex w-[90px] justify-center rounded-lg bg-red-100 align-middle font-medium text-red-600">
+      <ul className="ml-2 list-disc">
+        <li>Inactive</li>
+      </ul>
+    </div>
+  );
+}
--- front-end/src/pages/Users.jsx
+++ front-end/src/pages/Users.jsx
@@ -1,21 +1,26 @@
 import PageHeader from "../components/PageHeader";
+import Inactive from "../components/Inactive";
+import Active from "../components/Active";
+import { ArrowBigDownDash } from "lucide-react";
 
 export default function Users() {
   return (
     <>
       <PageHeader path="Dashboard / Users" header="Users" />
-      <div className="h-4/5 w-full rounded-xl border-2 bg-white">
+      <div className="h-4/5 w-full rounded-2xl border-[1px] bg-white">
         <table className="w-full table-auto">
           <thead>
-            <tr className="w-full bg-gray-100 text-left">
-              <th className="p-2 pl-5 pr-5 font-medium text-gray-700">Email</th>
-              <th className="p-2 pl-5 pr-5 font-medium text-gray-700">
-                Status
+            <tr className="rounded-xl bg-gray-100 text-left">
+              <th className="rounded-tl-xl p-2 pl-5 pr-5 font-medium text-gray-700">
+                Email Address
+              </th>
+              <th className="flex p-2 pl-5 pr-5 font-medium text-gray-700">
+                Status <ArrowBigDownDash className="ml-2" />
               </th>
               <th className="p-2 pl-5 pr-5 font-medium text-gray-700">
                 Last Signed In
               </th>
-              <th className="p-2 pl-5 pr-5 font-medium text-gray-700">
+              <th className="rounded-tr-xl p-2 pl-5 pr-5 font-medium text-gray-700">
                 Joined
               </th>
             </tr>
@@ -23,13 +28,33 @@ export default function Users() {
           <tbody>
             <tr className="border-b border-gray-300 text-left">
               <td className="p-5">johnDoe@gmail.com</td>
-              <td className="p-5">Online</td>
+              <td className="p-5">
+                <Active />
+              </td>
               <td className="p-5 font-light">06/08/2024 6:00PM</td>
               <td className="p-5 font-light">06/05/2024</td>
             </tr>
             <tr className="border-b border-gray-300 text-left">
               <td className="p-5">johnDoe@gmail.com</td>
-              <td className="p-5">Online</td>
+              <td className="p-5">
+                <Inactive />
+              </td>
+              <td className="p-5 font-light">06/08/2024 6:00PM</td>
+              <td className="p-5 font-light">06/05/2024</td>
+            </tr>
+            <tr className="border-b border-gray-300 text-left">
+              <td className="p-5">johnDoe@gmail.com</td>
+              <td className="p-5">
+                <Active />
+              </td>
+              <td className="p-5 font-light">06/08/2024 6:00PM</td>
+              <td className="p-5 font-light">06/05/2024</td>
+            </tr>
+            <tr className="border-b border-gray-300 text-left">
+              <td className="p-5">johnDoe@gmail.com</td>
+              <td className="p-5">
+                <Inactive />
+              </td>
               <td className="p-5 font-light">06/08/2024 6:00PM</td>
               <td className="p-5 font-light">06/05/2024</td>
             </tr>
