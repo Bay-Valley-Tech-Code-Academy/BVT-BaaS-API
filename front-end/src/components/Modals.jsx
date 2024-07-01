3d9d83fd95f065cca75d9861d75c95395f73b0a9
Manuel Manriquez <manriquezmanny@gmail.com>
2024-06-30 18:39:43 -0700

    Starting flowbite modal

---
+++ front-end/src/components/Modals.jsx
@@ -0,0 +1,47 @@
+import { Button, Modal } from "flowbite-react";
+import { useState } from "react";
+import { HiOutlineExclamationCircle } from "react-icons/hi";
+import { Trash2 } from "lucide-react";
+
+export function DeleteModal() {
+  const [openModal, setOpenModal] = useState(false);
+
+  return (
+    <>
+      <button
+        className="ml-4 border-none bg-transparent p-0"
+        onClick={() => setOpenModal(true)}
+      >
+        <Trash2 className="size-5 text-black hover:text-red-600" />
+      </button>
+      <Modal
+        show={openModal}
+        size="md"
+        onClose={() => setOpenModal(false)}
+        popup
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="mx-auto mb-4 h-14 w-14 text-gray-400 dark:text-gray-200" />
+            <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">
+              Are you sure you want to delete this project?
+            </h3>
+            <div className="flex justify-center gap-4">
+              <Button color="danger" onClick={() => setOpenModal(false)}>
+                Delete Project
+              </Button>
+              <Button color="gray" onClick={() => setOpenModal(false)}>
+                Cancel
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+    </>
+  );
+}
+
+export default {
+  DeleteModal,
+};
