86d1091829f2767ac7cbc134101d175ca06172ec
Alan Ibarra <alanjibarradev@gmail.com>
2024-07-05 09:53:04 -0700

    merge carlos landing page

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -3,21 +3,22 @@ import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import "./index.css";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import DashboardHome from "./pages/DashboardHome";
 import Projects from "./pages/Projects";
 import Settings from "./pages/Settings";
 import Users from "./pages/Users";
 import DashboardLayout from "./components/DashboardLayout";
 import LandingPage from "./components/LandingPage";
-<<<<<<< HEAD
 import AuthLayout from "./components/AuthLayout";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-=======
 
 const queryClient = new QueryClient();
->>>>>>> main
 
 const router = createBrowserRouter([
   {
@@ -25,13 +26,13 @@ const router = createBrowserRouter([
     element: <LandingPage />,
   },
   {
-    path: '/auth',
+    path: "/auth",
     element: <AuthLayout />,
     children: [
       { index: true, element: <Navigate to="/auth/login" replace /> },
       { path: "/auth/login", element: <Login /> },
-      { path: "/auth/signup", element: <SignUp /> }
-    ]
+      { path: "/auth/signup", element: <SignUp /> },
+    ],
   },
   {
     path: "/dashboard",
