f11a659ab6747562568fba34a077553e8c926597
Daniel-Galvezcohort238 <daniel.galvez.cohort238@gmail.com>
2024-07-17 10:45:12 -0700

    added MUI/X charts to homepage

--- front-end/src/components/HomeComponents/SuccessfulLogins.jsx
+++ front-end/src/components/HomeComponents/SuccessfulLogins.jsx
@@ -1,28 +1,59 @@
-import {Cloud, Check} from "lucide-react";
+import React from 'react';
+import { Calendar } from 'lucide-react';
+import { LineChart } from '@mui/x-charts/LineChart';
 
+const uData = [4000, 3000, 2000, 2780, 1890, 2390, 3490];
+const pData = [2400, 1398, 9800, 3908, 4800, 3800, 4300];
+const xLabels = [
+  'Page A',
+  'Page B',
+  'Page C',
+  'Page D',
+  'Page E',
+  'Page F',
+  'Page G',
+];
 
 export default function SettingsUserStorage() {
+    const [logins, setLogins] = React.useState(200);
+
+    const [loginsPeriod, setLoginsPeriod] = React.useState('This month');
+
     return (
             <div id="user-storage" className="flex flex-col justify-between bg-white rounded-2xl h-96 w-full p-4 mr-6">
                 <div className="flex flex-col">
-                    <div id="cloud-check" className="relative flex justify-center items-center rounded-1 text- bg-dashboard-gray-100 h-8 w-28 text-">
-                        This month
+                    <div id="logins-period" className="relative flex justify-center items-center rounded-md font-medium text-gray-400 bg-dashboard-gray-100 h-10 w-32 text-">
+                        <Calendar className='mr-1' size={20}/>This month
                     </div>
-                    <h1 className="font-bold text-xl">User Storage</h1>
-                    <p className="text-dashboard-gray-50">
-                        Supervise your user limit
-                    </p>
+                    <div id='login-analytics' className='flex'>
+                        <div id='login-analytics-left' className='mr-8'>
+
+                            <h1 className="font-bold text-4xl mt-6">{logins}</h1>
+                            <div id='logins-net' className='flex items-center '>
+
+                                <div className="text-sm font-medium text-gray-400 mr-28">
+                                    Logins
+                                </div>
+                                
+                            </div>
+                        </div>
+                        <div id='login-analyitcs-right'>
+                        <LineChart
+                            width={500}
+                            height={300}
+                            series={[
+                                { data: pData, label: 'pv' },
+                                { data: uData, label: 'uv' },
+                            ]}
+                            xAxis={[{ scaleType: 'point', data: xLabels }]}
+                        />
+                        <div id='login-chart-x-axis'>
+
+                        </div>
+                        </div>
+                    </div>
                 </div>
                     
-                <div id="progress-bar" className="w-full">
-                    <div className="flex justify-between">
-                        <div>0</div> <div>100</div>
-
-                    </div>
-                    <div className="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-                        <div className="bg-dashboard-purple-300 h-2.5 rounded-full" style={{width: 250}}></div>
-                    </div>
-                </div>
             </div>
 
             
