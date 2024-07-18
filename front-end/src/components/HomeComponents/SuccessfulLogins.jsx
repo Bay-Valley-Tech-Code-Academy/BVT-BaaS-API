52d91cf056527adfdf268926bcc30f195e00c326
Daniel-Galvezcohort238 <daniel.galvez.cohort238@gmail.com>
2024-07-18 15:59:13 -0700

    added to the calendars, and added a project selection element

--- front-end/src/components/HomeComponents/SuccessfulLogins.jsx
+++ front-end/src/components/HomeComponents/SuccessfulLogins.jsx
@@ -17,17 +17,37 @@ const xLabels = [
 ];
 
 export default function SettingsUserStorage() {
-    const [selected, setSelected] = React.useState();
-    const [loginsPeriod, setLoginsPeriod] = React.useState();
+    const [calendarShows, setCalendarShows] = React.useState(false);
+    const [range, setRange] = React.useState();
     
 
     const [logins, setLogins] = React.useState(200);
 
+    function calendarClicked () {
+        setCalendarShows(true);
+    }
+    function closeClicked () {
+        setCalendarShows(false);
+    }
+
     return (
-            <div id="user-storage" className="flex flex-col justify-between bg-white rounded-2xl h-96 w-full p-4 mr-6">
+            <div id="user-storage" className="relative flex flex-col justify-between bg-white rounded-2xl w-full h-96 p-4 mr-6">
                 <div className="flex flex-col">
-                    <div id="logins-period" className="relative flex justify-center items-center rounded-md font-medium text-gray-400 bg-dashboard-gray-100 h-10 w-32 text-">
-                        <DayPicker className='absolute drop-shadow-md bg-dashboard-gray-100 mr-1' size={2}/>This month
+
+                    <select name="" id="" className="
+                        relative flex justify-center items-center rounded-md font-medium text-gray-400 bg-dashboard-gray-100 h-10 w-32 cursor-pointer hover:bg-blue-50 
+                        hover:text-slate-700">
+                        <option value="">Project</option>
+                        <option value="">Project1</option>
+                    </select>
+                    <div 
+                        id="logins-period" 
+                        className="
+                            relative flex justify-center items-center rounded-md font-medium text-gray-400 bg-dashboard-gray-100 h-10 w-32 cursor-pointer hover:bg-blue-50 
+                            hover:text-slate-700"
+                        onClick={calendarClicked}
+                    >
+                        <Calendar className=' bg-dashboard-gray-100 mr-1' size={20}/>This month
                     </div>
                     <div id='login-analytics' className='flex'>
                         <div id='login-analytics-left' className='mr-8'>
@@ -57,7 +77,16 @@ export default function SettingsUserStorage() {
                         </div>
                     </div>
                 </div>
-                    
+                {(calendarShows) && <div> 
+                    <DayPicker mode='range' selected={range} onSelect={setRange} className='absolute top-0 left-0 drop-shadow-md bg-dashboard-gray-100 ' size={2}/>
+                    <button 
+                        onClick={closeClicked} 
+                        className='absolute top-[1.4rem] left-[10.75rem] pl-1 pr-1 border-solid border-2 rounded-3xl hover:bg-blue-100 hover:text-slate-700 hover:border-blue-100'
+                    >
+                        close
+                    </button>
+                </div>}
+
             </div>
 
             
--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      left: {
+        "42": "left: 12rem"
+      },
       colors: {
         dashboard: {
           gray: {
